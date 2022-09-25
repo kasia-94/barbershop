12cdbf306acedffc8012411e582ed6195d805149
kasia-94 <99141117+kasia-94@users.noreply.github.com>
2022-09-25 13:04:22 +0000

    Deploying to gh-pages from @ kasia-94/barbershop@14385dc4da1a003630e241a601b5b38c9ddf98a7 🚀

---
+++ index.5b9446f5.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-menu-modal-open]"),closeModalBtn:document.querySelector("[data-menu-modal-close]"),modal:document.querySelector("[data-menu-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.5b9446f5.js.map
---
+++ index.82d5a391.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-menu-price-open]"),closeModalBtn:document.querySelector("[data-price-modal-close]"),modal:document.querySelector("[data-price-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.82d5a391.js.map
